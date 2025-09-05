e80f70869bce79f3c5fe83aa9659a31b27c0f148
verico <191323186+ver1co@users.noreply.github.com>
2025-09-05 06:04:31 -0400

    Update a.sh

--- a.sh
+++ a.sh
@@ -13,9 +13,9 @@ echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 echo root:password | chpasswd
 
 pacman -Syu
-pacman -S sudo base-devel grub efibootmgr sddm linux-headers linux-api-headers linux-firmware-nvidia
-pacman -S alacritty nano nano-syntax-highlighting vlc wofi noto-fonts swaync dolphin nmap ffmpeg man-db man-pages bash bash-completion amd-ucode reflector
-pacman -S networkmanager firewalld flatpak dialog openssh openssl iwd wireplumber pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse
+pacman -S --noconfirm sudo base-devel grub efibootmgr sddm linux-headers linux-api-headers linux-firmware-nvidia
+pacman -S --noconfirm alacritty nano nano-syntax-highlighting vlc wofi noto-fonts swaync dolphin nmap ffmpeg man-db man-pages bash bash-completion amd-ucode reflector
+pacman -S --noconfirm networkmanager firewalld flatpak dialog openssh openssl iwd wireplumber pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse
 pacman -S --noconfirm nvidia nvidia-utils nvidia-settings 
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
