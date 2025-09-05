8a0f081faa27a1f53c788eef4c07426ff7c64614
verico <191323186+ver1co@users.noreply.github.com>
2025-09-05 06:05:38 -0400

    Update a.sh

--- a.sh
+++ a.sh
@@ -14,24 +14,3 @@ echo root:password | chpasswd
 
 pacman -Syu
 pacman -S --noconfirm sudo base-devel grub efibootmgr sddm linux-headers linux-api-headers linux-firmware-nvidia
-pacman -S --noconfirm alacritty nano nano-syntax-highlighting vlc wofi noto-fonts swaync dolphin nmap ffmpeg man-db man-pages bash bash-completion amd-ucode reflector
-pacman -S --noconfirm networkmanager firewalld flatpak dialog openssh openssl iwd wireplumber pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse
-pacman -S --noconfirm nvidia nvidia-utils nvidia-settings 
-
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-
-grub-mkconfig -o /boot/grub/grub.cfg
-
-systemctl enable NetworkManager
-systemctl enable bluetooth
-systemctl enable sshd
-systemctl enable reflector.timer
-systemctl enable firewalld
-
-useradd -mG wheel verico
-echo verico:password | chpasswd
-
-# external
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-curl -s https://ohmyposh.dev/install.sh | bash -s
